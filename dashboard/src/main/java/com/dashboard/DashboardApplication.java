72054ab0eda74aa75d1420227957ca14becf98f6
yuezht <75134974@qq.com>
2017-09-30 16:20:27 +0800

    add eureka client

--- dashboard/src/main/java/com/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/com/dashboard/DashboardApplication.java
@@ -2,8 +2,10 @@ package com.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DashboardApplication {
 
 	public static void main(String[] args) {
