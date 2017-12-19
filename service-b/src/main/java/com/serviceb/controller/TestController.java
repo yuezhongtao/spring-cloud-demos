10dd3d7567d7b7d03c8407600705a89badb2f430
yuezht <75134974@qq.com>
2017-12-19 17:56:36 +0800

    update

--- service-b/src/main/java/com/serviceb/controller/TestController.java
+++ service-b/src/main/java/com/serviceb/controller/TestController.java
@@ -4,14 +4,15 @@ import com.google.common.collect.Maps;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/test")
 public class TestController {
-    @RequestMapping(value = "b",method = RequestMethod.GET)
-    public Map<String,Object> test(){
+    @RequestMapping(value = "/b",method = RequestMethod.GET)
+    public @ResponseBody Map<String,Object> test(){
         Map<String,Object> map = Maps.newHashMap();
         map.put("result","success");
         return map;
